707757d1c1e6ccb7f95e390d94775ec89e174151
Deepak <dhimandeepak957@gmail.com>
2024-04-14 13:49:24 +0530

    Programmer D

--- src/app/blogs/post/layout.js
+++ src/app/blogs/post/layout.js
@@ -1,12 +1,8 @@
-import { usePathname } from "next/navigation"
-
 export const metadata = {
     title: "Blogs - Programmer D"
 }
 
 export default function BlogsLayout({ children }) {
-    // const pathname = usePathname();
-    // console.log(pathname)
     return (
       <div>
         {children}
